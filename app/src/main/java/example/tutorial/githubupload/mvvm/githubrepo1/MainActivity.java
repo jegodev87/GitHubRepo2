abc7f3fb6fc63a9530cd7cc371ab8e7c85ec9083
jegodev <jeev.chn@gmail.com>
2021-02-05 23:25:00 +0530

    Toast added

--- app/src/main/java/example/tutorial/githubupload/mvvm/githubrepo1/MainActivity.java
+++ app/src/main/java/example/tutorial/githubupload/mvvm/githubrepo1/MainActivity.java
@@ -12,5 +12,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Toast.makeText(this, "Git updated", Toast.LENGTH_SHORT).show();
+        Toast.makeText(this, "Edit Text added", Toast.LENGTH_SHORT).show();
     }
 }
