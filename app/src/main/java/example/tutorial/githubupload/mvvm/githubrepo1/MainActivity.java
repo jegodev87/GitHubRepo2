219e200d8c08edf233965f76772dd7e871762bd0
jegodev <jeev.chn@gmail.com>
2021-02-05 23:26:58 +0530

    button added

--- app/src/main/java/example/tutorial/githubupload/mvvm/githubrepo1/MainActivity.java
+++ app/src/main/java/example/tutorial/githubupload/mvvm/githubrepo1/MainActivity.java
@@ -13,5 +13,6 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toast.makeText(this, "Git updated", Toast.LENGTH_SHORT).show();
         Toast.makeText(this, "Edit Text added", Toast.LENGTH_SHORT).show();
+        Toast.makeText(this, "Added a button", Toast.LENGTH_SHORT).show();
     }
 }
