dc93534cdc1b50e409932fc47678cbbece7c6c04
jegodev <jeev.chn@gmail.com>
2021-02-09 04:57:52 +0530

    added expoplayer3

---
+++ app/src/main/java/example/tutorial/githubupload/mvvm/githubrepo1/ShowVideos.java
@@ -0,0 +1,15 @@
+package example.tutorial.githubupload.mvvm.githubrepo1;
+
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class ShowVideos extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_show_videos);
+    }
+}
