4fb586c28a28d124b95aa6831181ca1a626e2779
jegodev <jeev.chn@gmail.com>
2021-02-09 04:21:08 +0530

    added expoplayer 2

---
+++ app/src/main/java/example/tutorial/githubupload/mvvm/githubrepo1/models/ContentEntity.java
@@ -0,0 +1,32 @@
+package example.tutorial.githubupload.mvvm.githubrepo1.models;
+
+public class ContentEntity {
+
+    private String title;
+    private String contentUrl;
+    private String searchTitle;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getContentUrl() {
+        return contentUrl;
+    }
+
+    public void setContentUrl(String contentUrl) {
+        this.contentUrl = contentUrl;
+    }
+
+    public String getSearchTitle() {
+        return searchTitle;
+    }
+
+    public void setSearchTitle(String searchTitle) {
+        this.searchTitle = searchTitle;
+    }
+}
